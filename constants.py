1f5e348041889c3638dfd0ee10b257d1540d5b0f
arshith4401 <75303055+arshith4401@users.noreply.github.com>
2024-02-12 01:32:36 -0500

    Push code

---
+++ constants.py
@@ -0,0 +1,7 @@
+consumer_key= '4eKMxQOkJqNmtRsKPFJozxSg3'
+consumer_secret= '4r5aNCJSEHZWo5lCQqz4CrmGceLtCRSYeQcH5djPoLApTSfKRL'
+
+access_token='4690409384-q95MDTPEdKKWMVPQsoCWk315xctvhitjbAuIvlx'
+access_token_secret='pEGYs9dUIZVJNnY6oW2pM3yOXjIVa9ePxZw9Iiwh2nUKn'
+
+num_of_tweets = int(300)
